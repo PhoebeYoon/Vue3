a6fb13e9e5ff726d72ff05f3753b49bd7c282490
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-02-25 11:05:41 +0900

    Add files via upload

---
+++ lesson04/app04-1.js
@@ -0,0 +1,9 @@
+        Vue.createApp({
+            data(){
+                return {
+                    title :'The way back to home',
+                    author :'Spider man',
+                    pages : 100
+                }
+            }
+        }).mount('#app');
